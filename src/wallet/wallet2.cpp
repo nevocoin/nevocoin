9e69589d5f7166e4aad9922a030930674c749950
nevocoin <>
2023-09-17 15:48:44 +0000

    Fix wallet restore_from_height calculation

--- src/wallet/wallet2.cpp
+++ src/wallet/wallet2.cpp
@@ -3357,12 +3357,13 @@ void wallet2::pull_and_parse_next_blocks(bool first, bool try_incremental, uint6
         bool wallet_is_outdated = false;
         bool daemon_is_outdated = false;
         check_block_hard_fork_version(m_nettype, hf_version, height, wallet_is_outdated, daemon_is_outdated);
-        THROW_WALLET_EXCEPTION_IF(wallet_is_outdated || daemon_is_outdated, error::incorrect_fork_version,
-          "Unexpected hard fork version v" + std::to_string(hf_version) + " at height " + std::to_string(height) + ". " +
+
+        if (wallet_is_outdated || daemon_is_outdated) {
+          MWARNING("Unexpected hard fork version v" + std::to_string(hf_version) + " at height " + std::to_string(height) + ". " +
           (wallet_is_outdated
             ? "Make sure your wallet is up to date"
-            : "Make sure the node you are connected to is running the latest version")
-        );
+            : "Make sure the node you are connected to is running the latest version"));
+        }
       }
 
       parsed_blocks[i].o_indices = std::move(o_indices[i]);
@@ -12159,18 +12160,22 @@ uint64_t wallet2::get_daemon_blockchain_target_height(string &err)
 
 uint64_t wallet2::get_approximate_blockchain_height() const
 {
-  // time of v2 fork
-  const time_t fork_time = m_nettype == TESTNET ? 1448285909 : m_nettype == STAGENET ? 1520937818 : 1458748658;
-  // v2 fork block
-  const uint64_t fork_block = m_nettype == TESTNET ? 624634 : m_nettype == STAGENET ? 32000 : 1009827;
+  // time of fork
+  const time_t fork_time = m_nettype == 1694881880;
+  // block of fork
+  const uint64_t fork_block = 16;
   // avg seconds per block
   const int seconds_per_block = DIFFICULTY_TARGET_V2;
   // Calculated blockchain height
   uint64_t approx_blockchain_height = fork_block + (time(NULL) - fork_time)/seconds_per_block;
-  // testnet and stagenet got some huge rollbacks, so the estimation is way off
-  static const uint64_t approximate_rolled_back_blocks = m_nettype == TESTNET ? 342100 : 30000;
-  if ((m_nettype == TESTNET || m_nettype == STAGENET) && approx_blockchain_height > approximate_rolled_back_blocks)
+  // reduce blocks rollback to avoid
+  // "wallet's refresh-from-block-height setting is higher than daemon" bug
+  static const uint64_t approximate_rolled_back_blocks = 1000;
+  if (approx_blockchain_height > approximate_rolled_back_blocks) {
     approx_blockchain_height -= approximate_rolled_back_blocks;
+  } else {
+    approx_blockchain_height = 0;
+  }
   LOG_PRINT_L2("Calculated blockchain height: " << approx_blockchain_height);
   return approx_blockchain_height;
 }
